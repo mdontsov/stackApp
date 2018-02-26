47cbd9d78d01d7f5d3f35376f8ba76dc5105ead0
maksim.dontsov <maksim.dontsov@elisa.ee>
2018-02-26 08:39:30 +0200

    ...

--- src/main/java/com/example/stack/controller/StackController.java
+++ src/main/java/com/example/stack/controller/StackController.java
@@ -2,7 +2,6 @@ package com.example.stack.controller;
 
 import com.example.stack.model.StackApp;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
