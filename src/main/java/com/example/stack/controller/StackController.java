cc5f561865024c9959a797a144d41828bbab5375
maksim.dontsov <maksim.dontsov@elisa.ee>
2018-02-26 08:33:47 +0200

    ...

--- src/main/java/com/example/stack/controller/StackController.java
+++ src/main/java/com/example/stack/controller/StackController.java
@@ -6,36 +6,42 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
+@SessionAttributes("stackApp")
 public class StackController {
 
+	@ModelAttribute("stackApp")
+	public StackApp setStackForm() {
+		return new StackApp();
+	}
+
 	@RequestMapping("/")
-	public String stackForm(Model model) {
-		model.addAttribute("welcome", new StackApp());
+	public String stackForm() {
 		return "welcome";
 	}
 
-	@RequestMapping(value = "/", params = "push", method = RequestMethod.POST)
-	public String push(@ModelAttribute StackApp stackApp) {
+	@RequestMapping(value = "/", params = "push", method = RequestMethod.PUT)
+	public String push(@ModelAttribute("stackApp") StackApp stackApp) {
 		stackApp.doPush();
 		return "welcome";
 	}
 
 	@RequestMapping(value = "/", params = "pop", method = RequestMethod.POST)
-	public String pop(@ModelAttribute StackApp stackApp) {
+	public String pop(@ModelAttribute("stackApp") StackApp stackApp) {
 		stackApp.doPop();
 		return "welcome";
 	}
 
-	@RequestMapping(value = "/", params = "view", method = RequestMethod.POST)
-	public String view(@ModelAttribute StackApp stackApp) {
+	@RequestMapping(value = "/", params = "view", method = RequestMethod.GET)
+	public String view(@ModelAttribute("stackApp") StackApp stackApp) {
 		stackApp.doView();
 		return "welcome";
 	}
 
-	@RequestMapping(value = "/", params = "reset", method = RequestMethod.POST)
-	public String reset(@ModelAttribute StackApp stackApp) {
+	@RequestMapping(value = "/", params = "reset", method = RequestMethod.DELETE)
+	public String reset(@ModelAttribute("stackApp") StackApp stackApp) {
 		stackApp.doReset();
 		return "welcome";
 	}
